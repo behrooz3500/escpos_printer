22c9085f6beb0caf4a8ca2bb821e3cb8138a1d0d
behrooz3500 <behrooz3500@yahoo.com>
2023-06-11 23:06:13 +0330

    class for constants used in main

---
+++ ConsolePrinter/Constants.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePrinter
+{
+    public class Constants
+    {
+        // public string configPrinterName { get {return "printer_name";}}
+        public string configPrinterName => "printer_name";
+        public string configReportPath => "report_path";
+        public string configFileName => "//app.config";
+        public string licenseRelativePath => "//license.key";
+
+        public string stimulsoftBussinessObject => "receiptItems";
+        public string stimulsoftTimeVariable => "time";
+        public string stimulsoftDateVariavle => "date";
+
+
+        public enum ExitCode : int
+        {
+            /// <summary>
+            ///  Printing job exit codes
+            /// </summary>
+            
+            Success = 0,
+            PrinterNotFound = 11,
+            JsonNotFound = 12,
+            ReportNotFound = 13,
+            LicenseKeyMissed = 14,
+            UnknownError = 15,
+            NoArgumentFound = 16,
+        }
+    }
+}
