62a83cc366ac54065b03f2270b80167231687ae0
behrooz3500 <behrooz3500@yahoo.com>
2023-06-12 23:34:04 +0330

    added constants for stimulsoft variables

--- ConsolePrinter/Constants.cs
+++ ConsolePrinter/Constants.cs
@@ -11,12 +11,24 @@ namespace ConsolePrinter
         // public string configPrinterName { get {return "printer_name";}}
         public string configPrinterName => "printer_name";
         public string configReportPath => "report_path";
-        public string configFileName => "//app.config";
+        public string configFilePath => Environment.CurrentDirectory + "//config.json";
         public string licenseRelativePath => "//license.key";
 
+        public string stimulsoftReceiptItemsKey => "items_list";
         public string stimulsoftBussinessObject => "receiptItems";
-        public string stimulsoftTimeVariable => "time";
-        public string stimulsoftDateVariavle => "date";
+        public string stimulsoftReceiptDate => "receipt_date";
+        public string stimulsoftReceiptCode => "receipt_code";
+        public string stimulsoftCompanyTitle => "company_title";
+        public string stimulsoftCompanyPhone => "company_phone";
+        public string stimulsoftCompanyAddress => "company_address";
+        public string stimulsoftPersonName => "person_name";
+        public string stimulsoftPersonPhone => "person_phone";
+        public string stimulsoftPersonAddress => "person_address";
+        public string stimulsoftInitialPrice => "initial_price";
+        public string stimulsoftDiscount => "discount";
+        public string stimulsoftTax => "tax";
+        public string stimulsoftFinalPayment => "final_payment";
+        public string stimulsoftSummaryText => "summary_text";
 
 
         public enum ExitCode : int
